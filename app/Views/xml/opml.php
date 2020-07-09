f5e64b4746101a4e1109613b460bebd8b20a3363
Jason Morris <jasonmorris@gmail.com>
2020-07-08 21:02:59 -0400

    bmxfeed. -> BMXfeed

--- app/Views/xml/opml.php
+++ app/Views/xml/opml.php
@@ -9,10 +9,10 @@ header("Content-Type: text/xml");
 echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
 echo "<opml version=\"1.0\">\n";
 echo "<head>\n";
-echo "<title>bmxfeed opml</title>\n";
+echo "<title>BMXfeed OPML</title>\n";
 echo "</head>\n";
 echo "<body>\n";
-echo "<outline text=\"bmxfeed\">\n";
+echo "<outline text=\"BMXfeed\">\n";
 foreach ($sites as $row) {
   echo "<outline title=\"" . htmlspecialchars($row->site_name) . "\" text=\"" . htmlspecialchars($row->site_name) . "\" type=\"rss\" xmlUrl=\"" . htmlspecialchars($row->site_feed) . "\" htmlUrl=\"" . htmlspecialchars($row->site_url) . "\" />\n";
 }
