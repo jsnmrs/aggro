d1e42fac14e5c62af58733dee8673744dd345613
Jason Morris <jasonmorris@gmail.com>
2020-10-12 12:58:43 +0000

    Log clean (#39)
    * Error log clean - temporary exposure
    
    * Empty log check

--- app/Views/log.php
+++ app/Views/log.php
@@ -12,14 +12,16 @@ echo $this->include('includes/header'); ?>
     <div class="full">
       <h1>Log</h1>
       <ul>
-  <?php foreach ($build as $row) :?>
-    <?php if (is_object($row)) :?>
-    <li><?php echo $row->log_message; ?> <span class="ago" title="<?php echo $row->log_date; ?>"></span></li>
-    <?php endif; ?>
-    <?php if (is_string($row)) :?>
-      <li><?php echo $row; ?></li>
-    <?php endif; ?>
-  <?php endforeach; ?>
+  <?php if (is_array($build) || is_object($build)) :?>
+    <?php foreach ($build as $row) :?>
+      <?php if (is_object($row)) :?>
+      <li><?php echo $row->log_message; ?> <span class="ago" title="<?php echo $row->log_date; ?>"></span></li>
+      <?php endif; ?>
+      <?php if (is_string($row)) :?>
+        <li><?php echo $row; ?></li>
+      <?php endif; ?>
+    <?php endforeach; ?>
+  <?php endif; ?>
       </ul>
     </div>
   </div>
