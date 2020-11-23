42d8cdead3aa80e35114ae4fbb55a316f618a055
Jason Morris <jasonmorris@gmail.com>
2020-11-22 21:07:00 -0500

    Refactor vimeo method

--- app/Controllers/Aggro.php
+++ app/Controllers/Aggro.php
@@ -132,40 +132,43 @@ class Aggro extends BaseController {
     $aggroModel = new AggroModels();
     $vimeoModel = new VimeoModels();
 
-    if (gate_check()) {
-      if ($videoID == NULL) {
-        $data['stale'] = $aggroModel->getChannels(30, "vimeo", 5);
-
-        if ($data['stale'] != FALSE) {
-          foreach ($data['stale'] as $channel) {
-            $data['feed'] = vimeo_get_feed($channel->source_channel_id);
-            $data['number_added'] = $vimeoModel->parseChannel($data['feed']);
-
-            echo "\nAdded " . $data['number_added'] . " videos from " . $channel->source_name . ".\n";
-
-            if ($data['feed'] != FALSE || empty($data['feed'])) {
-              $aggroModel->updateChannel($channel->source_slug);
-            }
-          }
-        }
-      }
-
-      if ($videoID !== NULL) {
-        $videoID = esc($videoID);
-        if (!$aggroModel->checkVideo($videoID)) {
-          $request = "https://vimeo.com/api/v2/video/" . $videoID . ".json";
-          $result = fetch_url($request, 'json', 0);
-          foreach ($result as $item) {
-            $sourceID = str_replace('https://vimeo.com/', '', $item->user_url);
-          }
-
-          $data['feed'] = vimeo_get_feed($sourceID);
-          $data['number_added'] = $vimeoModel->parseChannel($data['feed'], $videoID);
-
-          echo "\nAdded https://vimeo.com/" . $videoID . " from " . $sourceID . ".\n";
-        }
-      }
+    if (!gate_check()) {
+      return FALSE;
     }
+
+    if ($videoID == NULL) {
+      $data['stale'] = $aggroModel->getChannels(30, "vimeo", 5);
+
+      if ($data['stale'] == FALSE) {
+        return FALSE;
+      }
+
+      foreach ($data['stale'] as $channel) {
+        $data['feed'] = vimeo_get_feed($channel->source_channel_id);
+        $data['number_added'] = $vimeoModel->parseChannel($data['feed']);
+        echo "\nAdded " . $data['number_added'] . " videos from " . $channel->source_name . ".\n";
+        $aggroModel->updateChannel($channel->source_slug);
+      }
+
+      return TRUE;
+    }
+
+    $videoID = esc($videoID);
+
+    if (!$aggroModel->checkVideo($videoID)) {
+      $request = "https://vimeo.com/api/v2/video/" . $videoID . ".json";
+      $result = fetch_url($request, 'json', 0);
+      $sourceID = str_replace('https://vimeo.com/', '', $result[0]->user_url);
+
+      $data['feed'] = vimeo_get_feed($sourceID);
+      $data['number_added'] = $vimeoModel->parseChannel($data['feed'], $videoID);
+
+      echo "\nAdded https://vimeo.com/" . $videoID . " from " . $sourceID . ".\n";
+
+      return TRUE;
+    }
+
+    return FALSE;
   }
 
   /**
