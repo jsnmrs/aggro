c56208710371a315feb6170c26e7d75b2c54e3e5
Jason Morris <jasonmorris@gmail.com>
2020-11-24 21:30:28 -0500

    Reduce complexity of video feed search and parser

--- app/Helpers/youtube_helper.php
+++ app/Helpers/youtube_helper.php
@@ -113,3 +113,64 @@ if (!function_exists('youtube_id_from_url')) {
   }
 
 }
+
+if (!function_exists('youtube_parse_meta')) {
+
+  /**
+   * Parse youtube video metadata for DB import.
+   *
+   * @param object $item
+   *   Node from RSS feed.
+   *
+   * @return array
+   *   Video metadata added.
+   */
+  function youtube_parse_meta(object $item) {
+    helper('aggro');
+    $video = [];
+
+    $now = date('Y-m-d H:i:s');
+    $archive = date('Y-m-d H:i:s', strtotime('-31 day', strtotime($now)));
+    $currentVideo = $item->get_item_tags('http://www.youtube.com/xml/schemas/2015', 'videoId');
+    $video['video_id'] = $currentVideo[0]['data'];
+    $video['aggro_date_added'] = $now;
+    $video['aggro_date_updated'] = $now;
+    $published = $item->get_item_tags('http://www.w3.org/2005/Atom', 'published');
+    $video['video_date_uploaded'] = date("Y-m-d H:i:s", strtotime($published[0]['data']));
+    $video['flag_bad'] = 0;
+    $video['flag_archive'] = 0;
+    $video['flag_tweet'] = 1;
+    $video['video_type'] = 'youtube';
+    if ($video['video_date_uploaded'] <= $archive) {
+      $video['flag_archive'] = 1;
+      $video['flag_tweet'] = 0;
+    }
+    $video['video_title'] = htmlentities($item->get_title(), ENT_QUOTES, 'utf-8', FALSE);
+    $group = $item->get_item_tags(SIMPLEPIE_NAMESPACE_MEDIARSS, 'group');
+    $community = $group[0]['child'][SIMPLEPIE_NAMESPACE_MEDIARSS]['community'];
+    $statistics = $community[0]['child'][SIMPLEPIE_NAMESPACE_MEDIARSS]['statistics'];
+    $video['video_plays'] = $statistics[0]['attribs']['']['views'];
+    $thumbnail = $group[0]['child'][SIMPLEPIE_NAMESPACE_MEDIARSS]['thumbnail'];
+    $video['video_thumbnail_url'] = $thumbnail[0]['attribs']['']['url'];
+    $channelID = $item->get_item_tags('http://www.youtube.com/xml/schemas/2015', 'channelId');
+    $video['video_source_id'] = $channelID[0]['data'];
+    $author = $item->get_item_tags('http://www.w3.org/2005/Atom', 'author');
+    $authorURL = $author[0]['child']['http://www.w3.org/2005/Atom']['uri'];
+    $video['video_source_url'] = $authorURL[0]['data'];
+    $authorName = $author[0]['child']['http://www.w3.org/2005/Atom']['name'];
+    $video['video_source_username'] = htmlentities($authorName[0]['data'], ENT_QUOTES, 'utf-8', FALSE);
+
+    $oEmbed = "https://www.youtube.com/oembed?format=xml&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D" . $video['video_id'];
+    $result = fetch_url($oEmbed, 'simplexml', 1);
+    $video['video_width'] = $result->width;
+    $video['video_height'] = $result->height;
+    $video['video_aspect_ratio'] = round($result->width / $result->height, 3);
+    $video['video_source_user_slug'] = "";
+    if (strpos($result->author_url, 'user/') !== FALSE) {
+      $video['video_source_user_slug'] = str_replace('https://www.youtube.com/user/', '', $result->author_url);
+    }
+
+    return $video;
+  }
+
+}
--- app/Models/VimeoModels.php
+++ app/Models/VimeoModels.php
@@ -10,23 +10,19 @@ use CodeIgniter\Model;
 class VimeoModels extends Model {
 
   /**
-   * Parse Vimeo feed for videos.
+   * Search Vimeo feed for specific video.
    *
    * @param object $feed
    *   Fetched YouTube feed.
-   * @param string $findVideo
+   * @param string $videoId
    *   Video ID to look for.
    *
-   * @return int
-   *   Number of videos added.
+   * @return bool
+   *   Video added.
    */
-  public function parseChannel($feed, $findVideo = FALSE) {
+  public function searchChannel($feed, $videoId) {
     $aggroModel = new AggroModels();
-    $utilityModel = new UtilityModels();
-    helper('aggro');
-    $addCount = 0;
-    $now = date('Y-m-d H:i:s');
-    $archive = date('Y-m-d H:i:s', strtotime('-31 day', strtotime($now)));
+    helper('vimeo');
 
     if ($feed === FALSE) {
       return FALSE;
@@ -34,61 +30,42 @@ class VimeoModels extends Model {
 
     if (is_array($feed) || is_object($feed)) {
       foreach ($feed as $item) {
-        $video = [];
-        $saveVideo = 0;
+        if ($videoId == $item->id && !$aggroModel->checkVideo($item->id)) {
+          $video = vimeo_parse_meta($item);
+          $aggroModel->addVideo($video);
 
-        // If looking for specific video.
-        if ($findVideo !== FALSE && $findVideo == $item->id) {
-          $saveVideo = 1;
+          return TRUE;
         }
+      }
+    }
 
-        // If video isn't in DB and not looking for specific.
-        if ($findVideo == FALSE && !$aggroModel->checkVideo($item->id)) {
-          $saveVideo = 1;
-        }
+    return FALSE;
+  }
 
-        if ($saveVideo == 1) {
-          $video['video_id'] = $item->id;
-          $video['aggro_date_added'] = $now;
-          $video['aggro_date_updated'] = $now;
-          $video['video_date_uploaded'] = date("Y-m-d H:i:s", strtotime($item->upload_date));
-          $video['flag_bad'] = 0;
-          $video['flag_archive'] = 0;
-          $video['flag_tweet'] = 1;
-          $video['video_type'] = 'vimeo';
-          if ($video['video_date_uploaded'] <= $archive) {
-            $video['flag_archive'] = 1;
-            $video['flag_tweet'] = 0;
-          }
-          $video['video_title'] = htmlentities($item->title, ENT_QUOTES, 'utf-8', FALSE);
-          $video['video_plays'] = 0;
-          if (isset($item->stats_number_of_plays)) {
-            $video['video_plays'] = $item->stats_number_of_plays;
-          }
-          $video['video_thumbnail_url'] = $item->thumbnail_large;
-          $video['video_source_id'] = str_replace('https://vimeo.com/', '', $item->user_url);
-          $video['video_source_url'] = $item->user_url;
-          $video['video_source_username'] = htmlentities($item->user_name, ENT_QUOTES, 'utf-8', FALSE);
-          $video['video_width'] = $item->width;
-          $video['video_height'] = $item->height;
-          $video['video_aspect_ratio'] = round($item->width / $item->height, 3);
-          $video['video_source_user_slug'] = $video['video_source_id'];
+  /**
+   * Parse Vimeo feed for videos.
+   *
+   * @param object $feed
+   *   Fetched YouTube feed.
+   *
+   * @return int
+   *   Number of videos added.
+   */
+  public function parseChannel($feed) {
+    $aggroModel = new AggroModels();
+    $utilityModel = new UtilityModels();
+    helper('vimeo');
+    $addCount = 0;
 
-          // Add $video to DB.
-          $sql = "INSERT INTO aggro_videos (video_id, aggro_date_added, aggro_date_updated, video_date_uploaded, flag_archive, flag_bad, video_plays, video_title, video_thumbnail_url, video_width, video_height, video_aspect_ratio, video_source_id, video_source_username, video_source_url, flag_tweet, video_type, video_source_user_slug) VALUES ('" . $video['video_id'] . "', '" . $video['aggro_date_added'] . "', '" . $video['aggro_date_updated'] . "', '" . $video['video_date_uploaded'] . "', " . $video['flag_archive'] . ", 0, " . $video['video_plays'] . ", '" . $video['video_title'] . "', '" . $video['video_thumbnail_url'] . "', " . $video['video_width'] . ", " . $video['video_height'] . ", " . $video['video_aspect_ratio'] . ", '" . $video['video_source_id'] . "', '" . $video['video_source_username'] . "', '" . $video['video_source_url'] . "', " . $video['flag_tweet'] . ", '" . $video['video_type'] . "', '" . $video['video_source_user_slug'] . "')";
+    if ($feed === FALSE) {
+      return FALSE;
+    }
 
-          $this->db->query($sql);
-
-          if ($video['flag_archive'] == 0 && fetch_thumbnail($video['video_id'], $video['video_thumbnail_url'])) {
-            $message = "Added https://vimeo.com/" . $video['video_id'] . ", fetched thumbnail.";
-          }
-
-          if ($video['flag_archive'] == 1) {
-            $message = "Added and archived https://vimeo.com/" . $video['video_id'] . ".";
-          }
-
-          $utilityModel->sendLog($message);
-
+    if (is_array($feed) || is_object($feed)) {
+      foreach ($feed as $item) {
+        if (!$aggroModel->checkVideo($item->id)) {
+          $video = vimeo_parse_meta($item);
+          $aggroModel->addVideo($video);
           $addCount++;
         }
       }
