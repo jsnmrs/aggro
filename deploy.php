68bf89f779dc75285e26f5c9880f81ad29cac639
Jason Morris <jasonmorris@gmail.com>
2020-11-26 15:29:00 -0500

    Deploy setup (#98)
    * Add deploy script
    
    * Rename tasks
    
    * Temporary switch to PR trigger for testing
    
    * Remove vendors step
    
    * Remove shared and writable steps
    
    * Place .env
    
    * Exclude more files from rsync
    
    * Plural gets me, everytime.
    
    * Add shared and writeable folder config
    
    * Remove unused codeigniter receipe
    
    * Add code igniter recipe back as test
    
    * Trigger with main branch push
    
    * change destination

---
+++ deploy.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * Aggro deploy script.
+ */
+
+namespace Deployer;
+
+require 'recipe/codeigniter.php';
+require 'recipe/rsync.php';
+
+set('application', 'aggro');
+// Speed up deployment.
+set('ssh_multiplexing', TRUE);
+
+set('writable_mode', 'chmod');
+// CodeIgniter shared dirs.
+set('shared_dirs', ['writable/cache', 'writable/logs']);
+// CodeIgniter writable dirs.
+set('writable_dirs', ['writable/cache', 'writable/logs']);
+
+set('rsync_src', function () {
+  // Project is in root.
+  return __DIR__;
+});
+
+add('rsync', [
+  'exclude' => [
+    '.browserslistrc',
+    '.cron*',
+    '.docksal',
+    '.editorconfig',
+    '.env*',
+    '.eslintrc',
+    '.git',
+    '.github',
+    '.gitignore',
+    '*.sql',
+    '.stylelintrc',
+    'build',
+    'composer.json',
+    'composer.lock',
+    'deploy.php',
+    '*DS_Store',
+    'LICENSE',
+    'node_modules',
+    'package.json',
+    'package-lock.json',
+    'phpcs.xml',
+    'phpmd.xml',
+    'phpunit.xml.dist',
+    'postcss.config.js',
+    'public/thumbs',
+    'README.md',
+    'tests',
+    'writable/cache',
+    'writable/debugbar',
+    'writable/logs',
+  ],
+]);
+
+// Copy dotenv file from github secret to server.
+task('deploy:secrets', function () {
+  file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
+  upload('.env', get('release_path'));
+});
+
+
+host('bmxfeed.com')
+  ->hostname('bmxfeed.com')
+  ->stage('production')
+  ->user('bmxfeed')
+  ->set('deploy_path', '/home/bmxfeed/aggro');
+
+after('deploy:failed', 'deploy:unlock');
+
+desc('Deploy the application');
+task('deploy', [
+  'deploy:info',
+  'deploy:prepare',
+  'deploy:lock',
+  'deploy:release',
+  'rsync',
+  'deploy:shared',
+  'deploy:writable',
+  'deploy:secrets',
+  'deploy:symlink',
+  'deploy:unlock',
+  'cleanup',
+]);
