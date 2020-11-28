025b4c5a1ea12ff7ce0e77920bc1108f0aeca6be
Jason Morris <jasonmorris@gmail.com>
2020-11-27 20:35:50 -0500

    Update about page, add link to repo

--- app/Views/about.php
+++ app/Views/about.php
@@ -11,9 +11,12 @@ echo $this->include('includes/header'); ?>
   <div class="wrap">
     <div class="full">
       <h1>About BMXfeed</h1>
-      <p>Launched in early 2006, BMXfeed has grown from a directory of links to a news and video aggregator for all things bmx.</p>
-      <p>BMXfeed is updated automatically with news and videos from all over the bmx world. There are no ads and no filters. It&rsquo;s better that way.</p>
-      <p><a href="/submit/">Get in touch if you have a site or video to submit</a>.</p>
+      <p>Launched in late 2006, BMXfeed is a bmx news and video aggregator (robot). The site finds and adds new blog posts and videos 24/7.</p>
+
+      <p>The code that runs BMXfeed is called aggro. The entire <a href="https://github.com/jsnmrs/aggro">aggro codebase is open source</a>.</p>
+
+      <p><a href="/submit/">Send me a note</a> if you have a site or video to submit.</p>
+
       <p>I&rsquo;m Jason. I built and maintain BMXfeed. See more of my work at <a href="https://jasonmorris.com/">jasonmorris.com</a>.</p>
     </div>
   </div>
