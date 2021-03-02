36e8c5c7011bf109d0233f817979dfe13fbac449
Jason Morris <jasonmorris@gmail.com>
2021-03-02 06:15:42 -0500

    Remove more trailing slashes

--- app/Views/xml/feed.php
+++ app/Views/xml/feed.php
@@ -11,14 +11,14 @@ echo "<channel>\n";
 echo "<title>BMXfeed</title>\n";
 echo "<link>https://bmxfeed.com</link>\n";
 echo "<description>BMXfeed directory updates</description>\n";
-echo "<atom:link href=\"https://bmxfeed.com/feed/\" rel=\"self\" type=\"application/rss+xml\" />\n";
+echo "<atom:link href=\"https://bmxfeed.com/feed\" rel=\"self\" type=\"application/rss+xml\" />\n";
 foreach ($build as $row) {
   echo "<item>\n";
   echo "<title>" . stripslashes($row->site_name) . " on BMXfeed</title>\n";
-  echo "<link>https://bmxfeed.com/sites/" . $row->site_slug . "/</link>\n";
+  echo "<link>https://bmxfeed.com/sites/" . $row->site_slug . "</link>\n";
   echo "<description>";
   echo "<![CDATA[<p>Updated <em>" . stripslashes($row->site_name) . "</em> on BMXfeed.";
-  echo " Check out <a href=\"https://bmxfeed.com/sites/" . $row->site_slug . "/\">https://bmxfeed.com/sites/" . $row->site_slug . "/</a> for more info.</p>]]>";
+  echo " Check out <a href=\"https://bmxfeed.com/sites/" . $row->site_slug . "\">https://bmxfeed.com/sites/" . $row->site_slug . "</a> for more info.</p>]]>";
   echo "</description>\n";
   echo "<guid isPermaLink=\"true\">https://bmxfeed.com/sites/" . $row->site_slug . "</guid>\n";
   echo "<pubDate>" . date("D, d M Y H:i:s O", strtotime($row->site_date_added)) . "</pubDate>\n";
