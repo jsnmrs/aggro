49e025679f85b41e66fcf436a62babd4ccd58446
Jason Morris <jasonmorris@fastmail.com>
2023-01-14 19:30:14 -0500

    Intellisense flagged this

--- app/Controllers/Aggro.php
+++ app/Controllers/Aggro.php
@@ -229,7 +229,7 @@ class Aggro extends BaseController {
 
     if (!$aggroModel->checkVideo($videoID)) {
       $request = "https://vimeo.com/api/v2/video/" . $videoID . ".json";
-      $result = fetch_url($request, 'json', 0);
+      $result = json_decode(fetch_url($request, 'json', 0));
       $sourceID = str_replace('https://vimeo.com/', '', $result[0]->user_url);
       $data['feed'] = vimeo_get_feed($sourceID);
       $data['number_added'] = $vimeoModel->searchChannel($data['feed'], $videoID);
