0386af98ed63af18817db69d6ab0717ec5c38d19
Jason Morris <code@jasonmorris.com>
2024-12-14 19:51:31 -0500

    Update contact

--- app/Views/submit.php
+++ app/Views/submit.php
@@ -4,8 +4,8 @@
   <div class="wrap">
     <div class="full">
       <h1>Submit</h1>
-      <p>BMXfeed does a pretty good job at finding websites and videos automatically, but it can't find everything. <strong>Do you have a site or video I should add?</strong></p>
-      <p>Call or text (240) 232-6851 and let me know what&rsquo;s up.</p>
+      <p>BMXfeed does a pretty good job at finding websites and videos automatically, but it can't find everything. <strong>Do you have a site or video it should add?</strong></p>
+      <p>Email <a href="mailto:jason@bmxfeed.com">jason@bmxfeed.com</a></p>
     </div>
   </div>
 </main>
