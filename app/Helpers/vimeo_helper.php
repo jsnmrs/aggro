b047d05748d48638d9e618dd2156eb22cff7259e
Jason Morris <code@jasonmorris.com>
2025-06-20 21:36:29 -0400

    Fix double HTML encoding of video titles
    Remove HTML encoding from API helpers and rely on view layer encoding only.
    This prevents titles with special characters from being double-encoded
    (e.g., & becoming &amp;amp; instead of &amp;).
    
    - Remove htmlentities() calls from vimeo_helper and youtube_helper
    - Update tests to expect raw values instead of encoded ones
    - Encoding now happens only at display time via esc() in views

--- app/Helpers/vimeo_helper.php
+++ app/Helpers/vimeo_helper.php
@@ -90,7 +90,7 @@ if (! function_exists('vimeo_parse_meta')) {
         if ($video['video_date_uploaded'] <= $archive) {
             $video['flag_archive'] = 1;
         }
-        $video['video_title'] = htmlentities($item->title, ENT_QUOTES, 'utf-8', false);
+        $video['video_title'] = $item->title;
         $video['video_plays'] = 0;
         if (isset($item->stats_number_of_plays)) {
             $video['video_plays'] = $item->stats_number_of_plays;
@@ -98,7 +98,7 @@ if (! function_exists('vimeo_parse_meta')) {
         $video['video_thumbnail_url']   = $item->thumbnail_large;
         $video['video_source_id']       = str_replace('https://vimeo.com/', '', $item->user_url);
         $video['video_source_url']      = $item->user_url;
-        $video['video_source_username'] = htmlentities($item->user_name, ENT_QUOTES, 'utf-8', false);
+        $video['video_source_username'] = $item->user_name;
         $video['video_width']           = $item->width;
         $video['video_height']          = $item->height;
         $video['video_aspect_ratio']    = round($item->width / $item->height, 3);
