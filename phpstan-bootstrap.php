d7d6f2eeb0d18a804a3af08ee3e65245100299ba
Jason Morris <code@jasonmorris.com>
2026-01-22 21:04:44 -0500

    Add phpstan bootstrap for CodeIgniter path constants
    Define FCPATH and ROOTPATH constants for static analysis,
    eliminating false positive errors in view files.

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * PHPStan bootstrap file.
+ *
+ * Defines CodeIgniter path constants that are set at runtime.
+ */
+defined('FCPATH')   || define('FCPATH', __DIR__ . '/public/');
+defined('ROOTPATH') || define('ROOTPATH', __DIR__ . '/');
